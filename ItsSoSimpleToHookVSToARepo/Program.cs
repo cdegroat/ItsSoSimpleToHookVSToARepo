3c710146c07033ba311f2acbb4655827e2c9e32d
colin <colingdegroat@gmail.com>
2021-11-01 18:00:23 -0400

    blah

--- ItsSoSimpleToHookVSToARepo/Program.cs
+++ ItsSoSimpleToHookVSToARepo/Program.cs
@@ -7,6 +7,8 @@ namespace ItsSoSimpleToHookVSToARepo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //make changes
         }
     }
 }
