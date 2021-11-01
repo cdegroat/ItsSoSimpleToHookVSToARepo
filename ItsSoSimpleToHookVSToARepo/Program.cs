5a9a41d02b9dabe3e4f2aa0ca0ab3b46f3628cd8
colin <colingdegroat@gmail.com>
2021-11-01 17:59:40 -0400

    Add project files.

---
+++ ItsSoSimpleToHookVSToARepo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ItsSoSimpleToHookVSToARepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
